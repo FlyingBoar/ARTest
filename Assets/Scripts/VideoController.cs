10522f56fa60706e5a34a5e5577c37c23994c174
Fulvio <montecristo310@gmail.com>
2019-05-29 18:42:03 +0200

    First commit

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+    private bool _isRendered;
+
+    public bool IsRendered
+    {
+        get { return _isRendered; }
+        set
+        {
+            if(_isRendered != value)
+            {
+                _isRendered = value;
+                ToggleVideo(IsRendered);
+            }
+        }
+    }
+
+
+    MeshRenderer rend;
+    VideoPlayer videoCtrl;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+        videoCtrl = GetComponent<VideoPlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rend.isVisible)
+            IsRendered = true;
+        else
+            IsRendered = false;
+    }
+
+    void ToggleVideo(bool _active)
+    {
+        if (_active)
+            videoCtrl.Play();
+        else
+            videoCtrl.Pause();
+    }
+}
